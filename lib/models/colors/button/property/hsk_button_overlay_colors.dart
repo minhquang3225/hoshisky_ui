6308124abeaa393690e30cce8f113d4206c2cda3
minhquang3225 <60904218+minhquang3225@users.noreply.github.com>
2025-05-10 20:38:34 +0700

    update

--- lib/models/colors/button/property/hsk_button_overlay_colors.dart
+++ lib/models/colors/button/property/hsk_button_overlay_colors.dart
@@ -1,4 +1,3 @@
-import 'package:hoshisky_ui/models/colors/hsk_colors.dart';
 import 'package:hoshisky_ui/models/colors/hsk_primary_colors.dart';
 
 abstract class HskButtonOverlayColors extends HskOrdinalColors {}
